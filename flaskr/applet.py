e55172ee8aea524d6e320fccf0d3a41b8bfd03ce
Pritkumar Godhani <pritgodhani132@gmail.com>
2023-02-27 17:07:15 +0530

    INIT: app built. awaiting enclave edl and functional code.

---
+++ flaskr/applet.py
@@ -0,0 +1,20 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('applet', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    # posts = db.execute(
+    #     'SELECT p.id, title, body, created, author_id, username'
+    #     ' FROM post p JOIN user u ON p.author_id = u.id'
+    #     ' ORDER BY created DESC'
+    # ).fetchall()
+    posts = []
+    return render_template('applet/index.html', posts=posts)
