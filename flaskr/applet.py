0b1a531ee9f24a244f1a6e0a1f19159b620f3339
Pritkumar Godhani <pritgodhani132@gmail.com>
2023-03-05 22:50:57 +0530

    Updated views and the interface. index is the access point now

--- flaskr/applet.py
+++ flaskr/applet.py
@@ -5,16 +5,36 @@ from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
+import sgx_call as sgx
 
 bp = Blueprint('applet', __name__)
 
-@bp.route('/')
+# @bp.route('/')
+# def index():
+#     db = get_db()
+#     # posts = db.execute(
+#     #     'SELECT p.id, title, body, created, author_id, username'
+#     #     ' FROM post p JOIN user u ON p.author_id = u.id'
+#     #     ' ORDER BY created DESC'
+#     # ).fetchall()
+#     posts = []
+#     return render_template('applet/index.html', posts=posts)
+
+@bp.route('/', methods=('GET', 'POST'))
+@login_required
 def index():
-    db = get_db()
-    # posts = db.execute(
-    #     'SELECT p.id, title, body, created, author_id, username'
-    #     ' FROM post p JOIN user u ON p.author_id = u.id'
-    #     ' ORDER BY created DESC'
-    # ).fetchall()
-    posts = []
-    return render_template('applet/index.html', posts=posts)
+    if request.method == "POST":
+        input_ = request.form['input']
+        error = None
+        if not input_:
+            error = "Cannot work with empty input."
+        
+        if error is not None:
+            flash(error)
+        else:
+            input_ = int(input_)
+            
+            output = 3*input_#sgx.csgx_mult3(input_)
+        
+            return render_template('applet/index.html', output=output)
+    return render_template('applet/index.html', output=None)
