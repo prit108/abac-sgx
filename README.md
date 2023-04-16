03be10b1cd3d76813972d9bf9c7c15701e29c747
Pritkumar Godhani <pritgodhani132@gmail.com>
2023-04-16 21:31:07 +0530

    Refined code and Added README

---
+++ README.md
@@ -0,0 +1,16 @@
+# APETEEt 
+
+This repository contains the source code for the APETEEt module and a Flask endpoint application to demonstrate its usage.
+
+
+The standalone PolTree Code is provided in the `PolTree` folder.
+
+The APETEEt Intel SGX Enclave Code is given in the `APETEEt_Enclave_Module` folder.
+
+The Flask App is provided with two endpoints `build` and `evaluate`.
+
+Complete the installation for Intel SGX SDK. Follow the instructions on Intel's official Github repository: [INTEL SGX SDK](https://github.com/intel/linux-sgx)
+
+Note that the following codes have be designed for Linux distros and tested on Ubuntu v22.04. 
+
+<em>Kindly note repository is under active development and changes might be made that temporarily affect the performance. We request you to be patient.</em>
--- flaskr/applet.py
+++ flaskr/applet.py
@@ -1,13 +1,17 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
+import json
+import os
+from ..mapper import build, evaluate_request
+from poltree import *
 
 bp = Blueprint('applet', __name__)
-
+poltree_dict = {}
 # @bp.route('/')
 # def index():
 #     db = get_db()
@@ -40,8 +44,40 @@ def index():
             flash(error)
         else:
             input_ = int(input_)
-            
-            output = sgx.csgx_mult3(input_)
-        
+            output_ = 3*input_
             return render_template('applet/index.html', output=output)
     return render_template('applet/index.html', output=None)
+
+
+@bp.route("/build", methods=('POST'))
+@login_required 
+def build_endpoint():
+    if request.method == "POST":
+        with request.files.items()[0] as value:
+            if value.content_type == 'application/json':
+                dst_filepath = os.path.join(os.getcwd()+"/received", value.filename)
+                if not os.path.exists(os.path.dirname(dst_filepath)):
+                    os.makedirs(os.path.dirname(dst_filepath))
+
+                with open(dst_filepath, 'wb') as fd:
+                    for chunk in value: 
+                        fd.write(chunk)
+                    print('Saved file as %s' % dst_filepath)
+                
+                global poltree_dict
+                poltree_dict[session["user_id"]] = build(dst_filepath)           
+        return render_template('applet/eval.html', output=None)
+
+@bp.route("/evaluate", methods=('GET'))
+@login_required 
+def evaluate_endpoint():
+    if request.method == "GET":
+        ua = request.form['ua']
+        oa = request.form['oa']
+        access = request.form['access']
+        
+        if session["user_id"] not in poltree_dict:
+            print("PolTree Not built.") 
+            return 0
+        else:
+            return evaluate_request(poltree_dict[session["user_id"]], ua, oa, access)
